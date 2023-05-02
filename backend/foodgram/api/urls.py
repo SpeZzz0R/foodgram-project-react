0c68605ba118d847f8c30a40826bb79c48c9c046
SpeZzz0R <vlad.z.lebron@gmail.com>
2023-05-02 20:52:27 +0300

    Create infrastructure

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -3,8 +3,7 @@ from rest_framework.routers import DefaultRouter
 
 from .views import (IngredientViewSet, RecipeViewSet, SelfUserView,
                     SetPasswordRetypeView, TagViewSet, UserViewSet,
-                    get_token
-)
+                    get_token)
 
 router = DefaultRouter()
 
