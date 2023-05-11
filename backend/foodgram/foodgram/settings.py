982a11be13b7d3ecc2201fdf2f289818285952a2
SpeZzz0R <vlad.z.lebron@gmail.com>
2023-05-12 00:30:03 +0300

    Create infrastructure

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -15,9 +15,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     '*',
-    'localhost',
-    '127.0.0.1',
-    '158.160.20.167',
+    # 'localhost',
+    # '127.0.0.1',
+    # '158.160.20.167',
 ]
 
 INSTALLED_APPS = [
